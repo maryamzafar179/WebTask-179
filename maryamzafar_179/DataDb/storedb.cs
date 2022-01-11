3a37f0d28fc934019b8f7bb2eabb078ef88621ec
Bakhtawar Habib <Bakhtawar Habib@DESKTOP-1LSIPP6>
2022-01-11 21:11:51 +0500

    Add project files.

---
+++ maryamzafar_179/DataDb/storedb.cs
@@ -0,0 +1,18 @@
+﻿using maryamzafar_179.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace maryamzafar_179.DataDb
+{
+    public class storedb : DbContext
+    {
+        public storedb(DbContextOptions<storedb>options):base(options)
+        {
+            
+        }
+        public DbSet<OrderModel> orders { get; set; }
+    }
+}
